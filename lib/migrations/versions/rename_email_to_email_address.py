dfbf1614ca7a75510beca6583904caba7a5a459d
jonah <kayiwa.jonah@student.moringaschool.com>
2024-03-23 12:57:45 +0300

    completed.

---
+++ lib/migrations/versions/rename_email_to_email_address.py
@@ -0,0 +1,19 @@
+""
+Revision ID: b4cfb074b436
+Revises: 791279dd1860
+Create Date: 2024-03-23 12:29:01.036020
+
+""
+from alembic import op
+import sqlalchemy as sa
+
+revision = 'b4cfb074b418'
+down_revision = '791279dd0760'
+branch_labels = None
+depends_on = None
+
+def upgrade() -> None:
+    pass
+
+def downgrade() -> None:
+    pass
